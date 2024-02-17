f4329f592951c3b6a702305320ad5c040a8d55dd
Maximebond007 <93667175+MaximeH2003@users.noreply.github.com>
2024-02-17 13:56:41 +0100

    Delete js directory

--- js/rid.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("ouvrirRideau").addEventListener("click", function() {
-    document.getElementById("rideau").style.display = "block";
-});
-
-document.getElementById("fermerRideau").addEventListener("click", function() {
-    document.getElementById("rideau").style.display = "none";
-});
-
